fb4840517b7eee2077ec985c4e5af0a916cd7f7e
BrunoGN91 <bruno.gazzera.nicolas@gmail.com>
2022-06-14 15:53:21 -0300

    Deployment issues

--- database/config/config.js
+++ database/config/config.js
@@ -1,23 +1,23 @@
 module.exports = {
   "development": {
-    "username": process.env.PORT_DB || "root",
-    "password": process.env.PASS_DB || null,
-    "database": process.env.NAME_DB || "node_db",
-    "host": process.env.USER_DB || "127.0.0.1",
+    "username": process.env.PORT_DB ,
+    "password": process.env.PASS_DB ,
+    "database": process.env.NAME_DB ,
+    "host": process.env.USER_DB ,
     "dialect": "mysql"
   },
   "test": {
-    "username": process.env.PORT_DB || "root",
-    "password": process.env.PASS_DB || null,
-    "database": process.env.NAME_DB || "node_db",
-    "host": process.env.USER_DB || "127.0.0.1",
+    "username": process.env.PORT_DB ,
+    "password": process.env.PASS_DB ,
+    "database": process.env.NAME_DB ,
+    "host": process.env.USER_DB ,
     "dialect": "mysql"
   },
   "production": {
-    "username": process.env.PORT_DB || "root",
-    "password": process.env.PASS_DB || null,
-    "database": process.env.NAME_DB || "node_db",
-    "host": process.env.USER_DB || "127.0.0.1",
+    "username": process.env.PORT_DB ,
+    "password": process.env.PASS_DB ,
+    "database": process.env.NAME_DB ,
+    "host": process.env.USER_DB ,
     "dialect": "mysql"
   }
 }
