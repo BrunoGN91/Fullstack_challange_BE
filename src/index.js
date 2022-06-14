e56c970a1f59d8e1e24fb2b04ebf53a272eb933c
BrunoGN91 <bruno.gazzera.nicolas@gmail.com>
2022-06-14 15:14:01 -0300

    Deployment update

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ http.globalAgent.maxSockets = 100
 
 app.use(methodOverride('_method'))
 app.use(cors(), function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", process.env.CORS_URL || "http://localhost:3000"); // update to match the domain you will make the request from
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
