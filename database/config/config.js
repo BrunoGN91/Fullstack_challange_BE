19dc986219ab16c14d71ad6c97cbc254871b0ba2
BrunoGN91 <bruno.gazzera.nicolas@gmail.com>
2022-06-16 14:06:37 -0300

    added Env cors atuth

--- database/config/config.js
+++ database/config/config.js
@@ -1,23 +1,23 @@
 module.exports = {
   "development": {
-    "username": process.env.PORT_DB ,
-    "password": process.env.PASS_DB ,
-    "database": process.env.NAME_DB ,
-    "host": process.env.USER_DB ,
+    "username": process.env.DB_USERNAME ,
+    "password": process.env.DB_PASSWORD ,
+    "database": process.env.DB_DATABASE ,
+    "host": process.env.DB_HOST ,
     "dialect": "mysql"
   },
   "test": {
-    "username": process.env.PORT_DB ,
-    "password": process.env.PASS_DB ,
-    "database": process.env.NAME_DB ,
-    "host": process.env.USER_DB ,
+    "username": process.env.DB_USERNAME ,
+    "password": process.env.DB_PASSWORD ,
+    "database": process.env.DB_DATABASE ,
+    "host": process.env.DB_HOST ,
     "dialect": "mysql"
   },
   "production": {
-    "username": process.env.PORT_DB ,
-    "password": process.env.PASS_DB ,
-    "database": process.env.NAME_DB ,
-    "host": process.env.USER_DB ,
+    "username": process.env.DB_USERNAME ,
+    "password": process.env.DB_PASSWORD ,
+    "database": process.env.DB_DATABASE ,
+    "host": process.env.DB_HOST ,
     "dialect": "mysql"
   }
 }
